a0b271a232896e9d706f8356c42096f8a2a44c3a
Reiji <88034696+forsyth47@users.noreply.github.com>
2023-03-16 23:47:32 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,12 +2,11 @@
 
 #printf "Enter the link: "
 #read link
+quality=480p
+#available qulities (360p/480p) for shinchan atleast (! dont forget to add p after 360/480 !)
 link=$1
-url=$(curl -s $link | grep "https://bayfiles.com/.*" | grep -o "360p.*" | cut -d\" -f4)
+url=$(curl -s $link | grep "https://bayfiles.com/.*" | grep -o "$quality.*" | cut -d\" -f4)
 for u in $url; do
     #echo "Downloading $u..."
-    while true; do
-	    curl -s $u | grep "https://.*.bayfiles.com.*" | tac | rev | cut -d\" -f2 | rev | sed -n 2p && break
-    	sleep 1
-    done
+    while true; do output=$(curl -s "$u"); [ -n "$output" ] && break; sleep 0.5; done; echo "$output" | grep "https://.*.bayfiles.com.*" | tac | rev | cut -d\" -f2 | rev | sed -n 2p
 done
