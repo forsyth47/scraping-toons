3c07bc16b7bf6e8515063731d7a177a6e646ad18
Reiji <88034696+forsyth47@users.noreply.github.com>
2023-03-17 02:26:10 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,5 +8,5 @@ link=$1
 url=$(curl -s $link | grep "https://bayfiles.com/.*" | grep -o "$quality.*" | cut -d\" -f4)
 for u in $url; do
     #echo "Downloading $u..."
-    while true; do output=$(curl -s "$u"); [ -n "$output" ] && break; sleep 0.5; done; echo "$output" | grep "https://.*.bayfiles.com.*" | tac | rev | cut -d\" -f2 | rev | sed -n 2p
+    while true; do output=$(curl -s "$u"); [ -n "$output" ] && break; sleep 0.5; done; echo "$output" | grep "https://.*.bayfiles.com.*" | tac | rev | cut -d\" -f2 | rev | while true; do output=$(curl -s "$u"); [ -n "$output" ] && break; sleep 0.5; done; echo "$output" | grep "https://.*.bayfiles.com.*" | tac | rev | cut -d\" -f2 | rev | grep -E 'https?://.*\.mp4(\b|$)' | sed -n '2p;2!{s/\.mp4.*$//;p;q}'
 done
