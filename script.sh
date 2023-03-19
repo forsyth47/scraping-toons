d6f45f80595c606059d7c41a1ccbf87d0b2f35a6
Reiji <88034696+forsyth47@users.noreply.github.com>
2023-03-20 00:21:55 +0530

    Create script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-#printf "Enter the link: "
-#read link
-quality=480p
-#available qulities (360p/480p) for shinchan atleast (! dont forget to add p after 360/480 !)
 link=$1
+quality=360p
+echo "Set Quality in script. Current set quality is: " $quality
+#available qulities (360p/480p) for shinchan atleast (! dont forget to add p after 360/480 !)
+
+pwd=$(pwd)
+mkdir ToonsDownloaded-$quality
+cd ToonsDownloaded-$quality
+
 url=$(curl -s $link | grep "https://bayfiles.com/.*" | grep -o "$quality.*" | cut -d\" -f4)
 for u in $url; do
     #echo "Downloading $u..."
@@ -12,7 +16,11 @@ for u in $url; do
     links=$(while true; do output=$(curl -s "$u"); [ -n "$output" ] && break; sleep 0.5; done; echo "$output" | grep "https://.*.bayfiles.com.*" | tac | rev | cut -d\" -f2 | rev | grep -E 'https?://.*\.mp4(\b|$)')
     for link in $links; do
     	if [ $(echo $link | cut -d\/ -f4) == $code ]; then
-        	echo $link
+        	#while true; do output=$(curl "$link"); [ -n "$output" ] && break; sleep 0.5; done; echo "$output"
+		while true; do wget $link -O $(echo $link | rev | cut -d\/ -f1 | rev) && break; sleep 1; done > /dev/null
+		#echo $link
     	fi
     done
 done
+
+cd $pwd
